ad8b315d5bc2cc946aea5512aa6b265cdadfbd3a
Anthony Wertz <anthony.wertz@gmail.com>
2020-03-26 10:54:06 -0400

    Removed h5py and added audata to setup_env script.

--- auviewer/static/scripts/setup_env.sh
+++ auviewer/static/scripts/setup_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-conda create -y --name medview python=3.6 flask cython h5py psutil simplejson
+conda create -y --name medview python=3.6 flask cython psutil simplejson
 conda activate medview
 pip install --upgrade pip
 pip install htmlmin watchdog Flask-User==0.6.21
@@ -12,3 +12,8 @@ pip install flask-socketio
 
 # For realtime files
 pip install watchdog
+
+# audata needed for the server.
+git clone https://github.com/autonlab/audata.git
+cd audata
+./rebuild
