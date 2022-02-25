00212b2d69e655a55348017bf354282d53833c90
Gus Welter <gwelter@andrew.cmu.edu>
2022-02-24 18:25:36 -0600

    Version bump.

--- auviewer/__init__.py
+++ auviewer/__init__.py
@@ -1,15 +1,15 @@
 """AUViewer package."""
 
-__VERSION_MAJOR__ = '0'
-__VERSION_MINOR__ = '1'
-__VERSION_BUILD__ = '1'
+__VERSION_MAJOR__ = '1'
+__VERSION_MINOR__ = '0'
+__VERSION_BUILD__ = '0'
 
 __DEBUG = False
 if __DEBUG:
     import datetime as dt
     __VERSION_TAGS__ = f"d{dt.datetime.now().strftime('%Y%m%d_%H%M')}"
 else:
-    __VERSION_TAGS__ = 'rc12'
+    __VERSION_TAGS__ = ''
 
 __VERSION__ = f'{__VERSION_MAJOR__}.{__VERSION_MINOR__}.{__VERSION_BUILD__}{__VERSION_TAGS__}'
 
